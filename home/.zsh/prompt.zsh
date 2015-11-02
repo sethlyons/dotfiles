84ae8aa0d05b093978ea14195c89c06d24164a28
Seth Lyons <slyons@fxcm.com>
2015-11-02 13:11:34 +0000

    incorporate fixes; only include file change hash if git status --porcelain has output

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -4,19 +4,41 @@ unset RPS1 RPROMPT
 autoload -U colors
 colors
 
-# Checks if working tree is dirty
-# From robbyrussell/oh-my-zsh
-parse_git_dirty() {
-  #for line in "$(command git status --porcelain)"; do
-  #  local info=("${(s/:/)string}")
-  #done
-
-  GIT_STATUS=$(command git status --porcelain 2>/dev/null)
-  if [[ -n $GIT_STATUS ]]; then
-    echo "1"
-  else
-    echo ""
+prompt_git_status() {
+  if ! $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
+    return 0
   fi
+
+  local dirty=0
+  local ref=$(git symbolic-ref HEAD 2>/dev/null || git show-ref --head -s --abbrev | head -n 1 2>/dev/null)
+  local summary
+  typeset -A summary
+
+  gsp=$(git status --porcelain)
+
+  if [[ -n $gsp ]]; then
+    for line in "${(@f)$(command git status --porcelain)}"; do
+      local gitstat=$line[(w)1]
+      summary[$gitstat]=$((summary[$gitstat] + 1))
+      dirty=1
+    done
+  fi
+
+  local color
+  if [[ $dirty -eq 1 ]]; then
+    color="yellow"
+  else
+    color="green"
+  fi
+
+  echo -n " git: %{$fg_bold[$color]%}${ref/refs\/heads\//}%{%f%b%} "
+
+
+  for k in "${(@k)summary}"; do
+    echo -n "[$k:$summary[$k]] "
+  done
+
+  return 1
 }
 
 prompt_rc() {
@@ -35,27 +57,14 @@ prompt_context() {
 prompt_git() {
   local color
 
-  if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
-    dirty=$(parse_git_dirty)
-    ref=$(git symbolic-ref HEAD 2>/dev/null || git show-ref --head -s --abbrev | head -n 1 2>/dev/null)
-
-    if [[ -n $dirty ]]; then
-      color="yellow"
-    else
-      color="green"
-    fi
-
-    #echo -n "%{$fg_bold[$color]%}${ref/refs\/heads\//}$dirty%{%f%b%} "
-    echo "%{$fg_bold[$color]%}${ref/refs\/heads\//}%{%f%b%} "
-  fi
 }
 
 prompt_git_branch() {
   local color
 
   if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
-    dirty=$(parse_git_dirty)
     ref=$(git symbolic-ref HEAD 2>/dev/null || git show-ref --head -s --abbrev | head -n 1 2>/dev/null)
+    dirty=$(command git status --porcelain 2>/dev/null)
 
     if [[ -n $dirty ]]; then
       color="yellow"
@@ -73,10 +82,19 @@ prompt_end() {
 
 build_prompt() {
   RETVAL=$?
-  #prompt_git
+
+  # first optional line
+  prompt_git_status
+  git_rc=$?
+
+  if [[ $git_rc -eq 1 ]]; then
+    echo ""
+  fi
+
+  # actual prompt
   prompt_rc
   prompt_context
-  prompt_git_branch
+  #prompt_git_branch
   prompt_end
 }
 
