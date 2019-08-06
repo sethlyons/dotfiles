d0cd03085b556d080490600b535d73e4671bfa0d
Seth Lyons <seth.lyons@gmail.com>
2019-08-05 20:44:19 -0400

    remove existing file before linking

--- createlinks.sh
+++ createlinks.sh
@@ -7,6 +7,9 @@ cd $dirname/home
 dir=$(pwd)
 dotfiles=$(find . -maxdepth 1 | awk -F/ '{ print $2 }')
 for file in $dotfiles; do
+  if [[ -f $HOME/$file ]]; then
+    rm $HOME/$file
+  fi
   ln -s $dir/$file ~/$file
 done
 
